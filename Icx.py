70ce7bc544708f79de6afd5e0672ddc1567e0c7d
Thalis Rebouças de Oliveira <50429343+thalisreboucas@users.noreply.github.com>
2024-09-13 17:39:24 -0300

    Add files via upload

--- Icx.py
+++ Icx.py
@@ -5,6 +5,7 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 
+# ------------------- Cliente -------------------#
 class ListarCliente:
     def __init__(self,host):
         # Load environment variables from .env file
@@ -24,7 +25,7 @@ class ListarCliente:
         auth_header = 'Basic {}'.format(base64.b64encode(token_bytes).decode('utf-8'))
         return auth_header
 
-    def get_client_data(self, page=1, records_per_page=20):
+    def executar(self, page=1, records_per_page=20):
         """Fetches client data from the API."""
         payload = {
             'qtype': 'cliente.id',
@@ -62,7 +63,7 @@ class ListarClienteContrato:
         auth_header = 'Basic {}'.format(base64.b64encode(token_bytes).decode('utf-8'))
         return auth_header
 
-    def listar_cliente_contrato(self, page=1, records_per_page=20):
+    def executar(self, page=1, records_per_page=20):
         """Fetches cliente_contrato data from the API."""
         payload = {
             'qtype': 'cliente_contrato.id',
@@ -81,6 +82,7 @@ class ListarClienteContrato:
         else:
             response.raise_for_status()
 
+# ------------------- Produtos ------------------#
 class ListarProdutos:
     def __init__(self, host):
         # Load environment variables from .env file
@@ -100,7 +102,7 @@ class ListarProdutos:
         auth_header = 'Basic {}'.format(base64.b64encode(token_bytes).decode('utf-8'))
         return auth_header
 
-    def listar_cliente_contrato(self, page=1, records_per_page=20):
+    def executar(self, page=1, records_per_page=20):
         """Fetches cliente_contrato data from the API."""
         payload = {
             'qtype': 'produtos.id',
@@ -119,7 +121,7 @@ class ListarProdutos:
         else:
             response.raise_for_status()
 
-
+# ------------------- Fornecedores --------------#
 class ListarFornecedores:
     def __init__(self, host):
         # Load environment variables from .env file
@@ -139,7 +141,7 @@ class ListarFornecedores:
         auth_header = 'Basic {}'.format(base64.b64encode(token_bytes).decode('utf-8'))
         return auth_header
 
-    def listar_cliente_contrato(self, page=1, records_per_page=20):
+    def executar(self, page=1, records_per_page=20):
         """Fetches cliente_contrato data from the API."""
         payload = {
             'qtype': 'fornecedor.id',
@@ -158,8 +160,165 @@ class ListarFornecedores:
         else:
             response.raise_for_status()
 
+#------------------- Provedor -------------------#
+class ListarPlanodeVelocidade:
+    def __init__(self, host):
+        # Load environment variables from .env file
+        load_dotenv()
+        self.host = host
+        self.base_url = f"https://{self.host}.com.br/webservice/v1/radgrupos"
+        self.token = os.getenv('API_TOKEN')
+        self.headers = {
+            'ixcsoft': 'listar',
+            'Authorization': self._get_auth_header(),
+            'Content-Type': 'application/json'
+        }
+
+    def _get_auth_header(self):
+        """Generates the Authorization header from the token."""
+        token_bytes = self.token.encode('utf-8')
+        auth_header = 'Basic {}'.format(base64.b64encode(token_bytes).decode('utf-8'))
+        return auth_header
+
+    def executar(self, page=1, records_per_page=20):
+        """Fetches cliente_contrato data from the API."""
+        payload = {
+            'qtype': 'radgrupos.id',
+            'query': '0',
+            'oper': '>',
+            'page': str(page),
+            'rp': str(records_per_page),
+            'sortname': 'radgrupos.id',
+            'sortorder': 'asc'
+        }
+
+        response = requests.post(self.base_url, data=payload, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.text  # You can process it further, like converting to JSON if needed
+        else:
+            response.raise_for_status()
+
+class ListarClientesFibra:
+    def __init__(self, host):
+        # Load environment variables from .env file
+        load_dotenv()
+        self.host = host
+        self.base_url = f"https://{self.host}.com.br/webservice/v1/radpop_radio_cliente_fibra"
+        self.token = os.getenv('API_TOKEN')
+        self.headers = {
+            'ixcsoft': 'listar',
+            'Authorization': self._get_auth_header(),
+            'Content-Type': 'application/json'
+        }
+
+    def _get_auth_header(self):
+        """Generates the Authorization header from the token."""
+        token_bytes = self.token.encode('utf-8')
+        auth_header = 'Basic {}'.format(base64.b64encode(token_bytes).decode('utf-8'))
+        return auth_header
+
+    def listar_cliente_fibra(self, page=1, records_per_page=20):
+        """Fetches cliente_contrato data from the API."""
+        payload = {
+            'qtype': 'radpop_radio_cliente_fibra.id',
+            'query': '0',
+            'oper': '>',
+            'page': str(page),
+            'rp': str(records_per_page),
+            'sortname': 'radpop_radio_cliente_fibra.id',
+            'sortorder': 'asc'
+        }
+
+        response = requests.post(self.base_url, data=payload, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.text  # You can process it further, like converting to JSON if needed
+        else:
+            response.raise_for_status()
+
+# ------------------- Suporte -------------------#
+class ListarOrdemdeServico:
+    def __init__(self, host):
+        # Load environment variables from .env file
+        load_dotenv()
+        self.host = host
+        self.base_url = f"https://{self.host}.com.br/webservice/v1/su_oss_chamado"
+        self.token = os.getenv('API_TOKEN')
+        self.headers = {
+            'ixcsoft': 'listar',
+            'Authorization': self._get_auth_header(),
+            'Content-Type': 'application/json'
+        }
+
+    def _get_auth_header(self):
+        """Generates the Authorization header from the token."""
+        token_bytes = self.token.encode('utf-8')
+        auth_header = 'Basic {}'.format(base64.b64encode(token_bytes).decode('utf-8'))
+        return auth_header
+
+    def executar(self, page=1, records_per_page=20):
+        """Fetches cliente_contrato data from the API."""
+        payload = {
+            'qtype': 'su_oss_chamado.id',
+            'query': '0',
+            'oper': '>',
+            'page': str(page),
+            'rp': str(records_per_page),
+            'sortname': 'su_oss_chamado.id',
+            'sortorder': 'asc'
+        }
+
+        response = requests.post(self.base_url, data=payload, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.text  # You can process it further, like converting to JSON if needed
+        else:
+            response.raise_for_status()
+
+class ListarAtendimento:
+    def __init__(self, host):
+        # Load environment variables from .env file
+        load_dotenv()
+        self.host = host
+        self.base_url = f"https://{self.host}.com.br/webservice/v1/su_ticket"
+        self.token = os.getenv('API_TOKEN')
+        self.headers = {
+            'ixcsoft': 'listar',
+            'Authorization': self._get_auth_header(),
+            'Content-Type': 'application/json'
+        }
+
+    def _get_auth_header(self):
+        """Generates the Authorization header from the token."""
+        token_bytes = self.token.encode('utf-8')
+        auth_header = 'Basic {}'.format(base64.b64encode(token_bytes).decode('utf-8'))
+        return auth_header
+
+    def executar(self, page=1, records_per_page=20):
+        """Fetches cliente_atendimentos data from the API."""
+        payload = {
+            'qtype': 'su_ticket.id',
+            'query': '0',
+            'oper': '>',
+            'page': str(page),
+            'rp': str(records_per_page),
+            'sortname': 'su_ticket.id',
+            'sortorder': 'asc'
+        }
+
+        response = requests.post(self.base_url, data=payload, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.text  # You can process it further, like converting to JSON if needed
+        else:
+            response.raise_for_status()
+
+
+
+
 
 # Usage example:
-#api_client = ListarCliente('NomeDaEmpresa')
-#df = api_client.get_client_data(page=1, records_per_page=20)
-#print(df)
+api_client = ListarAtendimento('SuaEmpresa')
+df = api_client.executar(page=1, records_per_page=20)
+print(df)
